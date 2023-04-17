e6ef74c4263656b12d293ae1420df7baa33c3e1d
Valentinusz <bb20020402@gmail.com>
2023-04-17 10:21:14 +0200

    improve ui

--- xd-foci/resources/views/home.blade.php
+++ xd-foci/resources/views/home.blade.php
@@ -1,12 +1,9 @@
 <x-app-layout>
-    <header role='banner'
-            class='bg-[url("../../public/images/pancho.jpg")] bg-center h-96 py-16 text-center bg-fixed bg-cover
-            border-b-indigo-600 border-b-4'
-    >
+    <header role='banner' class='h-96 py-16'>
             <h1 class="text-7xl font-bold">FociFociFoci</h1>
-            <h2 class='text-5xl my-4 te'>Minden ami foci egy helyen.</h2>
+            <h2 class='text-5xl my-4'>Minden ami foci egy helyen.</h2>
     </header>
-    <h2 class='text-center py-16 text-3xl'>Merülj el a labdarúgás világában!</h2>
+    <h3 class='text-center py-16 text-3xl'>Merülj el a labdarúgás világában!</h3>
         <div class='flex gap-8 justify-between mx-8'>
             <x-home-link label='Mérkőzések' icon='sports_soccer' :resource='route("games.index")'></x-home-link>
             <x-home-link label='Csapatok' icon='groupr' :resource='route("games.index")'></x-home-link>
