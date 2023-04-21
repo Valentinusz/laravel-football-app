d416c09d5c95c5aedab3a4723a1887a8c59efa71
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-04-21 20:49:21 +0200

    Create view

---
+++ xd-foci/resources/views/add-player.blade.php
@@ -0,0 +1,41 @@
+@php /** @var \App\Models\Team $team */ @endphp
+
+<x-app-layout>
+    <x-short-banner title='Új játékos'></x-short-banner>
+    <div class='mx-32'>
+        <form class='createForm' method='POST' action='{{ route('teams.players.store', $team->id) }}'>
+            @csrf
+            <!-- Team -->
+            <div>
+                <input id='team_id' name='team_id' type='number' value='{{ $team->id }}' hidden=''>
+                <x-input-error :messages=" $errors->get('team_id') "/>
+            </div>
+
+            <!-- Name -->
+            <div>
+                <label for='name'>Név</label>
+                <input id='name' name='name' value='{{ old('name') }}'>
+                <x-input-error :messages=" $errors->get('name') "/>
+            </div>
+
+
+            <!-- Number -->
+            <div>
+                <label for='number'>Mezszám</label>
+                <input id='number' name='number' type='number' value='{{ old('number') }}'>
+                <x-input-error :messages=" $errors->get('number') "/>
+            </div>
+
+            <!-- Birthdate -->
+            <div>
+                <label for='birthdate'>Születési dátum</label>
+                <input id='birthdate' name='birthdate' type='date' value='{{ old('number') }}'>
+                <x-input-error :messages=" $errors->get('birthdate') "/>
+            </div>
+
+            <button type='submit'>
+                Játékos hozzáadása
+            </button>
+        </form>
+    </div>
+</x-app-layout>
