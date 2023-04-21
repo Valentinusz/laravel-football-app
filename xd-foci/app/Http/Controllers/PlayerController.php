a1efb601face1da6fa81048db114550690477096
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-04-21 20:49:06 +0200

    Add authorization

--- xd-foci/app/Http/Controllers/PlayerController.php
+++ xd-foci/app/Http/Controllers/PlayerController.php
@@ -2,32 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Player;
+use App\Models\Team;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
-class PlayerController extends Controller
-{
+class PlayerController extends Controller {
+
+    // add authorization
+    public function __construct() {
+        $this->authorizeResource(Player::class, 'player');
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
+    public function index() {
+
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
+    public function create(Team $team) {
+        return view('add-player', ['team' => $team]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request): RedirectResponse {
+        $validated = $request->validate([
+            'team_id' => ['required', 'exists:teams,id'],
+            'name' => ['required'],
+            'number' => ['required', 'numeric', 'integer'],
+            'birthdate' => ['required', 'date']
+        ]);
+
+        Player::create($request->all());
+
+        return to_route('teams.show', ['team' => $request->team_id]);
     }
 
     /**
