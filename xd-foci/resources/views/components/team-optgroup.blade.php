82fe19eb5d7e2e90d7d035f7f088d5362c39527a
Valentinusz <bb20020402@gmail.com>
2023-04-21 08:07:10 +0200

    Add component
    Add component for displaying them embers of a team sorted by their number in an optgroup of a select.

---
+++ xd-foci/resources/views/components/team-optgroup.blade.php
@@ -0,0 +1,11 @@
+@props(['team'])
+@php
+use App\Models\Player;
+    /** @var \App\Models\Team $team */
+@endphp
+
+<optgroup label='{{ $team->name }}'>
+    @foreach( $team->players->sort(function (Player $a, Player $b) { return $a->number <=> $b->number; }) as $player )
+        <option value='{{ $player->id }}'>{{ $player->number }} – {{ $player->name }}</option>
+    @endforeach
+</optgroup>
