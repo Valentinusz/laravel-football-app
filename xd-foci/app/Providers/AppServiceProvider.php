3d29ed04458b0eb1deb8ac4ff56616c2e324787a
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-04-22 17:14:10 +0200

    Register admin deny response

--- xd-foci/app/Providers/AppServiceProvider.php
+++ xd-foci/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Access\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +18,9 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot(): void {
+        \Illuminate\Support\Facades\Response::macro('admin', function() {
+            return Response::deny("Ez a funkció csak admin jogosultságú felhasználók számára érhető el");
+        });
     }
 }
