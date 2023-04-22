13c15da440a0fa0322bc200bb6399a8e633b0554
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-04-22 15:09:12 +0200

    Remove auto-registered policy

--- xd-foci/app/Providers/AuthServiceProvider.php
+++ xd-foci/app/Providers/AuthServiceProvider.php
@@ -4,21 +4,15 @@ namespace App\Providers;
 
 use App\Models\Event;
 use App\Policies\EventPolicy;
-use Illuminate\Support\Facades\Gate;
-use App\Models\Game;
-use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
-class AuthServiceProvider extends ServiceProvider
-{
+class AuthServiceProvider extends ServiceProvider {
     /**
      * The model to policy mappings for the application.
      *
      * @var array<class-string, class-string>
      */
-    protected $policies = [
-        Event::class => EventPolicy::class
-    ];
+    protected $policies = [];
 
     /**
      * Register any authentication / authorization services.
