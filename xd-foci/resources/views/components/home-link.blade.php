36919fbb65323c0b69357f9864d1380b1c5ac2d8
Valentinusz <bb20020402@gmail.com>
2023-04-24 21:25:02 +0200

    Update home view

--- xd-foci/resources/views/components/home-link.blade.php
+++ xd-foci/resources/views/components/home-link.blade.php
@@ -1,6 +1,6 @@
 @props(['icon', 'label', 'resource'])
 
 <a class='home-item' href='{{ $resource }}'>
-    <span class="material-icons-outlined large">{{ $icon }}</span>
-    <h3 class='text-2xl'>{{ $label }}</h3>
+    <span class="material-icons-outlined xl">{{ $icon }}</span>
+    <h3 class='text-3xl'>{{ $label }}</h3>
 </a>
