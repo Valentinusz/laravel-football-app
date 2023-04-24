a974780e8cc2125799e90c0439c56ae632b9e4e8
Valentinusz <bb20020402@gmail.com>
2023-04-25 00:59:58 +0200

    Remove unused components

--- xd-foci/resources/views/components/short-banner.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['height', 'title'])
-
-<header role='banner'
-        class='bg-[url("../../public/images/pancho.jpg")] bg-center h-64 py-16 text-center bg-fixed bg-cover
-        border-b-indigo-600 border-b-4'
->
-        <h1 class="text-7xl font-bold">{{ $title }}</h1>
-        {{ $slot }}
-</header>
