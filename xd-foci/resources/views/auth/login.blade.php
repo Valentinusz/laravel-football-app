91732bacce0e332850d4f396ee98f32ae11809bd
Valentinusz <bb20020402@gmail.com>
2023-04-25 11:58:00 +0200

    Edit view

--- xd-foci/resources/views/auth/login.blade.php
+++ xd-foci/resources/views/auth/login.blade.php
@@ -32,7 +32,12 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-4">
+            <a  class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                href='{{route('register')}}'>
+                Regisztráció
+            </a>
+
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                     Elfelejtett jelszó
